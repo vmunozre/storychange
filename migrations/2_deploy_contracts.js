f28ed021b2e84b3d63daaed91d27f05b2dd0e905
gonzarugil@gmail.com <gonzarugil@gmail.com>
2018-03-03 17:57:07 +0100

    contract working v0.1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var StoryChain = artifacts.require("./StoryChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(StoryChain);
+
 };
