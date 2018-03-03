0d35cb6dfb17d03d84ce778210dd6301903434c2
Victor Reiner <vicmunozr@gmail.com>
2018-03-03 14:21:32 +0100

    Component writing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import {MetaModule} from './meta/meta.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MzButtonModule, MzInputModule } from 'ng2-materialize';
+import { AppComponent } from "./app.component";
+import { MetaModule } from "./meta/meta.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MzButtonModule, MzInputModule } from "ng2-materialize";
+import { WritingComponent } from "./writing/writing.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, WritingComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -24,4 +23,4 @@ import { MzButtonModule, MzInputModule } from 'ng2-materialize';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/writing/writing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-writing',
+  templateUrl: './writing.component.html',
+  styleUrls: ['./writing.component.css']
+})
+export class WritingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
