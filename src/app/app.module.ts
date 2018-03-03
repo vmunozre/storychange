42136b78360bca90fd7d958d49d9df818e2683fd
Guillermo Araujo Riestra <gar@MacBook-Pro-de-Guillermo.local>
2018-03-03 13:38:38 +0100

    Unboxing

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import {MetaModule} from './meta/meta.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MzButtonModule, MzInputModule } from 'ng2-materialize';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MetaModule,
+    BrowserAnimationsModule,
+    MzButtonModule,
+    MzInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ truffle.js
@@ -0,0 +1,14 @@
+module.exports = {
+    networks: {
+         development: {
+              host: "localhost",
+              port: 8545,
+              network_id: "*" // Match any network id
+            },
+          ganache: {
+              host: "localhost",
+              port: 7545,
+              network_id: "*" // Match any network id
+         },
+       }
+};
